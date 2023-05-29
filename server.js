0ca5d474f4af18a818fb34561e7fb2bd1cfb0c94
Shayan <shayan.bayat@elev.ga.lbs.se>
2023-05-29 09:51:44 +0200

    fixed copyright

--- server.js
+++ server.js
@@ -23,7 +23,11 @@ pool.query(`
     username VARCHAR(255) NOT NULL UNIQUE,
     password VARCHAR(255) NOT NULL
   )
-`);
+`, (err) => {
+  if (err) {
+    console.error(err);
+  }
+});
 
 // Serve static files from the "public" folder
 app.use(express.static(path.join(__dirname, 'public')));
@@ -73,6 +77,57 @@ app.post('/login', (req, res) => {
     }
   });
 });
+
+// Get the user's timezone
+const userTimeZone = Intl.DateTimeFormat().resolvedOptions().timeZone;
+
+// Require the needed files
+const { countries, zones } = require("moment-timezone/data/meta/latest.json");
+
+var timeZoneCityToCountry = {}; // Declare the variable here
+
+// Create a mapping of city to country
+Object.keys(zones).forEach((z) => {
+  const cityArr = z.split("/");
+  const city = cityArr[cityArr.length - 1];
+  timeZoneCityToCountry[city] = countries[zones[z].countries[0]].name;
+});
+
+console.log(timeZoneCityToCountry);
+
+// Define the mapping of city to country (if you want to override the previous mapping)
+timeZoneCityToCountry = {
+  // ... (city to country mapping)
+};
+
+var userRegion;
+
+if (Intl) {
+  // Get the user's timezone and extract the region, city, and country
+  const userTimeZone = Intl.DateTimeFormat().resolvedOptions().timeZone;
+  const timezone = userTimeZone.split("/");
+  userRegion = timezone[0];
+  console.log(timezone);
+}
+
+// If the user is vnot in Europe, show the popup when a button is clicked
+if (userRegion != "Europe") {
+  app.get('/popup', (req, res) => {
+    var popupContent =
+      "<p>Dear user, by using the website you are agreeing to your country's GDPR laws.</p>";
+
+    // Access the values from timeZoneCityToCountry and add them to the popup content
+    for (var city in timeZoneCityToCountry) {
+      var country = timeZoneCityToCountry[city];
+      popupContent += "<p>" + city + ": " + country + "</p>";
+    }
+
+    res.send(`<script>alert("${popupContent}"); window.location.href="/";</script>`);
+  });
+}
+
+console.log("Time Zone:", userTimeZone);
+console.log("Region:", userRegion);
 
 // Start the server
 app.listen(3000, () => {
